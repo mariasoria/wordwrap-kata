776fb97c9962b100c010da595df2dedbd57c44b0
Maria Soria <maria.it3@gmail.com>
2023-05-17 11:22:55 +0100

    Refactor. Extract method to fill line. Sort methods

--- src/Text.ts
+++ src/Text.ts
@@ -14,13 +14,12 @@ export class Text {
         return new Text(value);
     }
 
-    concat(otherText: Text): Text {
-        const text = this.text + otherText.value();
-        return Text.createText(text);
-    }
-
     fitsIn(columnWidth: ColumnWidth) {
         return this.text.length <= columnWidth.value();
+    }
+
+    public value(): string {
+        return this.text;
     }
 
     remainingText(aColumnWidth: ColumnWidth) {
@@ -29,14 +28,19 @@ export class Text {
     }
 
     wrapLine(aColumnWidth: ColumnWidth) {
-        let index = 0;
+        const index = 0;
         const words = this.text.split(' ');
         const wordHasToBeSplit = words[index].length > aColumnWidth.value();
         if (wordHasToBeSplit) {
             const newText = this.text.substring(0, aColumnWidth.value()) + '\\n'
             return Text.createText(newText);
         }
+        const line = this.fillLine(words, index, aColumnWidth);
+        const newText = this.text.substring(0, line.length) + '\\n'
+        return Text.createText(newText);
+    }
 
+    private fillLine(words: string[], index: number, aColumnWidth: ColumnWidth) {
         let line = words[index];
         while (this.isThereSpaceAvailableInTheLine(line, aColumnWidth) && this.thereAreMoreWords(words, index)) {
             index++;
@@ -48,23 +52,23 @@ export class Text {
                 break;
             }
         }
-        const newText = this.text.substring(0, line.length) + '\\n'
-        return Text.createText(newText);
+        return line;
     }
 
     private isThereSpaceAvailableInTheLine(line: string, aColumnWidth: ColumnWidth) {
         return line.length <= aColumnWidth.value();
     }
 
-    formatToCRLF(): string {
-        return this.value().replace(/\\n/g, '\r\n');
+    concat(otherText: Text): Text {
+        const text = this.text + otherText.value();
+        return Text.createText(text);
     }
 
     private thereAreMoreWords(words: string[], index: number) {
         return words.length > index + 1;
     }
 
-    public value(): string {
-        return this.text;
+    formatToCRLF(): string {
+        return this.value().replace(/\\n/g, '\r\n');
     }
 }
